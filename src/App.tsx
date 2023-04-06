924e4126c267f434672a367a323e776d4c9ab225
Yohana Fernandes <yohana@yotec.eti.br>
2023-04-06 17:16:22 -0300

    feat: create routes and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Box, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+import { appTheme } from './config/theme';
+import { Routes, Route, Link } from 'react-router-dom';
 
-import Button from '@mui/material/Button';
+const Home = () => (
+  <Box>
+    <Typography variant='h3' component='h1'>
+      Home
+    </Typography>
+  </Box>
+);
 
-export default function MyApp() {
+const About = () => (
+  <Box>
+    <Typography variant='h3' component='h1'>
+      About
+    </Typography>
+  </Box>
+)
+
+export function App() {
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <ThemeProvider theme={appTheme}>
+      <Box
+        component='main'
+        sx={{
+          height: '100vh',
+          backgroundColor: (theme) => theme.palette.grey[900],
+        }}
+      >
+        <Header />
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='about' element={<About />} />
+          </Routes>
+        </Layout>
+      </Box>
+    </ThemeProvider >
   );
 }
+
+export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import { Box, Container } from '@mui/material'
+import React from 'react'
+
+export function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <Box>
+            <Container
+                maxWidth='lg'
+                sx={{
+                    color: 'white',
+                    mt: 4,
+                    mb: 4,
+                }}
+            >
+                {children}
+            </Container>
+        </Box>
+    );
+}
